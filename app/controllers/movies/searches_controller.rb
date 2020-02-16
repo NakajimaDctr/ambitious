4d878c7697cf9d1ca7c83cb2a96527039d495e95
NakajimaDctr <drcardhms@gmail.com>
2020-02-16 23:40:35 +0900

    チェックボックスによる検索実装

--- app/controllers/movies/searches_controller.rb
+++ app/controllers/movies/searches_controller.rb
@@ -5,7 +5,19 @@ class Movies::SearchesController < ApplicationController
 
   private
   def search_params
-    params.permit(:category, :item, :performer_status, :performer_name, :music_title, :music_artist, :performed_at, :tags).merge(user_id: current_user.id)
+
+    # 学生/プロ・アマ
+    performer_status = ""
+    if params[:performer_status_student] == "1" && params[:performer_status_proama] == "1"
+      performer_status = ""
+    elsif params[:performer_status_student] == "1"
+      performer_status = "学生"
+    elsif params[:performer_status_proama] == "1"
+      binding.pry
+      performer_status = "プロ/アマ"
+    end
+
+    params.permit(:category, :item, :performer_name, :music_title, :music_artist, :performed_at, :tags).merge(user_id: current_user.id, performer_status: performer_status)
   end
 
 end
