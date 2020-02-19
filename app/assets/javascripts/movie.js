1a95b814df37daab8d98b1f1c5be94828aba468f
NakajimaDctr <drcardhms@gmail.com>
2020-02-19 17:50:44 +0900

    戻る機能実装
    ①セッションに検索条件設定
    ②戻るボタンでindexへ
    ③indexアクション内で検索
    ④セッションを元に検索、フォーム復元

--- app/assets/javascripts/movie.js
+++ app/assets/javascripts/movie.js
@@ -86,7 +86,7 @@ $(function(){
                     <div id='player_${movie.id}'>
                     </div>
                   </div>
-                  <a href="/movies/${movie.id}">
+                  <a href="/movies/${movie.id}" class='result__movie__show--link'>
                     <table class='result__movie__information'>
                       <tr>
                         <th>区分</th>
@@ -114,12 +114,12 @@ $(function(){
                       </tr>
                     </table>
                   </a>
-                    <a href="/movies/${movie.id}/edit">
-                      <div class='result__movie__edit-button'>
-                        <i class="far fa-edit .result__movie__edit-button__icon"></i>
-                        編集
-                      </div>
-                    </a>
+                  <a href="/movies/${movie.id}/edit" class='result__movie__edit--link'>
+                    <div class='result__movie__edit-button'>
+                      <i class="far fa-edit .result__movie__edit-button__icon"></i>
+                      編集
+                    </div>
+                  </a>
                 </div>`
 
     // 検索結果表示エリアに追加
@@ -158,4 +158,107 @@ $(function(){
       player.cueVideoById({videoId: id});
     }
   })
+
+  // 一覧から画面遷移する場合、検索条件を取得する
+  $(document).on("click", ".header__add-movie__btn,.result__movie__show--link,.result__movie__edit--link", function(){
+    
+    // 現在のページがindexの場合のみ（新規登録ボタンはindex以外でも使えるため）
+    if($(".search").length && $(".result").length){
+
+      // 検索フォームの状態を取得する
+      let category = "";
+      if($('input[name="category"]:checked').length){
+        category = $('input[name="category"]:checked').val();
+      }
+      let item = $('#item').val();
+      let performer_status_student = $('#performer_status_student').prop("checked");
+      let performer_status_proama = $('#performer_status_proama').prop("checked");
+      let performer_name = $('#performer_name').val();
+      let music_title = $('#music_title').val();
+      let music_artist = $('#music_artist').val();
+      let performed_at = $('#performed_at').val();
+      let tags = $('#tags').val();
+
+      // パラメータの配列化
+      let params_array = [
+        `category=${category}`,
+        `item=${item}`,
+        `performer_status_student=${performer_status_student}`,
+        `performer_status_proama=${performer_status_proama}`,
+        `performer_name=${performer_name}`,
+        `music_title=${music_title}`,
+        `music_artist=${music_artist}`,
+        `performed_at=${performed_at}`,
+        `tags=${tags}`
+      ];
+
+      // リンク先にパラメータを追加する
+      let params = '?' + params_array.join('&');
+      $(this).attr("href", $(this).attr("href") + params)
+    }
+  })
+
+  // 検索条件復元 & 動画プレイヤー埋め込み（APIの準備ができたら実行）
+  window.onYouTubePlayerAPIReady = function() {
+    // indexの場合、検索条件復元
+    if($(".search").length && $(".result").length){
+
+      // 区分ラジオボタン
+      $('input[name="category"]').val([$("#category_hidden").val()]);
+
+      // 種目
+      $("#item").val($("#item_hidden").val());
+
+      // 学生チェックボックス
+      if($("#performer_status_student_hidden").val() == "true"){
+        $("#performer_status_student").prop('checked', true);
+      }
+
+      // プロアマチェックボックス
+      if($("#performer_status_proama_hidden").val() == "true"){
+        $("#performer_status_proama").prop('checked', true);
+      }
+
+      // 演者名
+      $("#performer_name").val($("#performer_name_hidden").val());
+
+      // 曲名
+      $("#music_title").val($("#music_title_hidden").val());
+
+      // アーティスト
+      $("#music_artist").val($("#music_artist_hidden").val());
+
+      // 出演・受賞
+      $("#performed_at").val($("#performed_at_hidden").val());
+
+      // タグ
+      $("#tags").val($("#tags_hidden").val());
+
+      // 検索結果を取得
+      let movies = $(".result__movie")
+
+      if(movies.length != 0){
+        movies.each(function(){
+
+          // 動画埋め込み処理
+
+          // 動画情報のid
+          let movieId = $(this).find("[id^='movie_id_']").val()
+          // 挿入先となるdivのid
+          let playerId = "player_" + movieId;
+          // youtubeの動画ID("v="以降の11桁)
+          let videoId = $(this).find("[id^='movie_url_']").val().match(/v=.*/)[0].substring(2,13);
+
+          // プレイヤーの生成（１件）
+          player = new YT.Player(
+            playerId, {
+              width: "320",
+              height: "180",
+              videoId: videoId
+            }
+          )
+        });
+      }
+    };
+  }
 });
--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,42 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:show, :edit, :update]
-
+  before_action :set_params_to_session, only: [:new, :show, :edit]
   def index
+
+    # セッションに検索条件が保存されている場合
+    if session[:search_params]
+
+      # セッションからパラメータを取得する
+      search_params = session[:search_params]
+
+      # 検索フォーム復元用ハッシュ
+      @last_form = search_params
+
+      # 検索用パラメータを作成
+      params = create_search_params(search_params)
+
+      # 検索処理
+      @movies = current_user.movies.search(params)
+
+      # セッション削除
+      reset_session
+
+    else
+      # セッションに検索条件が保存されていない場合
+      # 検索フォーム復元用ハッシュを初期化する
+      @last_form = {
+        category: '',
+        item: '',
+        performer_status_student: '',
+        performer_status_proama: '',
+        performer_name: '',
+        music_title: '',
+        music_artist: '',
+        performed_at: '',
+        tags: ''
+      }
+    end
   end
 
   def new
@@ -54,8 +88,68 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
   end
 
+  # 現在のユーザーと動画に紐づくユーザーが一致しない場合、indexへ遷移させる
   def move_to_index
     user_id = @movie.user.id
     redirect_to action: :index if user_id != current_user.id
+  end
+
+  # 遷移前の検索条件をハッシュ化し、sessionに格納する
+  def set_params_to_session
+    session[:search_params] = {
+      category: params[:category],
+      item: params[:item],
+      performer_status_student: params[:performer_status_student],
+      performer_status_proama: params[:performer_status_proama],
+      performer_name: params[:performer_name],
+      music_title: params[:music_title],
+      music_artist: params[:music_artist],
+      performed_at: params[:performed_at],
+      tags: params[:tags]
+    }
+  end
+
+  # session削除
+  def reset_session
+    session.delete(:search_params) if session[:search_params]
+  end
+
+
+  # sessionのparamsから検索用のパラメータを作成する
+  def create_search_params(session_params)
+
+    # 演技区分の設定
+    category = ''
+    if session_params['category'] == 'stage'
+      category = 'ステージ'
+    elsif session_params['category'] == 'close'
+      category = 'クロース'
+    elsif session_params['category'] == 'salon'
+      category = 'サロン'
+    end
+
+    # 演者区分の設定
+    performer_status = ''
+    if session_params['performer_status_student'] == 'true' && session_params['performer_status_proama'] == 'true'
+      performer_status = ''
+    elsif session_params['performer_status_student'] == 'true'
+      performer_status = '学生'
+    elsif session_params['performer_status_proama'] == 'true'
+      performer_status = 'プロ・アマ'
+    end
+
+    # 検索用のパラメータ
+    params = {
+      category: category,
+      item: session_params['item'],
+      performer_status: performer_status,
+      performer_name: session_params['performer_name'],
+      music_title: session_params['music_title'],
+      music_artist: session_params['music_artist'],
+      performed_at: session_params['performed_at'],
+      tags: session_params['tags'],
+      user_id: current_user.id
+    }
+    return params
   end
 end
