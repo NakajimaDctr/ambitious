daf6d7beb03dd318b6ddea88c0df7ada443e1d33
NakajimaDctr <drcardhms@gmail.com>
2020-02-28 17:28:46 +0900

    Userモデルの単体テスト（追加）

--- spec/modules/user_spec.rb
+++ spec/modules/user_spec.rb
@@ -9,16 +9,78 @@ describe 'Userモデルの単体テスト' do
         # バリデーション結果の確認
         expect(user).to be_valid
       end
+      it "パスワードが6文字以上の場合、登録できる" do
+        # ダミーインスタンス生成
+        user = build(:user, password: "123456", password_confirmation: "123456")
+        # バリデーション結果の確認
+        expect(user).to be_valid
+      end
     end
 
     context '登録失敗' do
       it "nameが未入力の場合、登録できない" do
         # ダミーインスタンス生成
-        user = build(:user, name: nil)
+        user = build(:user, name: "")
         # バリデーション実行
         user.valid?
-        # エラーメッセージを確認
+        # エラーメッセージ生成を確認
         expect(user.errors[:name]).to include("を入力してください")
+      end
+
+      it "emailが未入力の場合、登録できない" do
+        # ダミーインスタンス生成
+        user = build(:user, email: "")
+        # バリデーション実行
+        user.valid?
+        # エラーメッセージ生成を確認
+        expect(user.errors[:email]).to include("を入力してください")
+      end
+
+      it "passwordが未入力の場合、登録できない" do
+        # ダミーインスタンス生成
+        user = build(:user, password: "")
+        # バリデーション実行
+        user.valid?
+        # エラーメッセージ生成を確認
+        expect(user.errors[:password]).to include("を入力してください")
+      end
+
+      it "password（確認用）が未入力の場合、登録できない" do
+        # ダミーインスタンス生成
+        user = build(:user, password_confirmation: "")
+        # バリデーション実行
+        user.valid?
+        # エラーメッセージ生成を確認
+        expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+      end
+
+      it "passwordが6文字未満の場合、登録できない" do
+        # ダミーインスタンス生成
+        user = build(:user, password: "12345", password_confirmation: "12345")
+        # バリデーション実行
+        user.valid?
+        # エラーメッセージ生成を確認
+        expect(user.errors[:password]).to include("は6文字以上で入力してください")
+      end
+
+      it "nameが重複している場合、登録できない" do
+        # ダミーインスタンス生成・登録
+        user = create(:user)
+        another_user = build(:user, name: user.name, email: "test2@gmail.com")
+        # バリデーション実行
+        another_user.valid?
+        # エラーメッセージ生成を確認
+        expect(another_user.errors[:name]).to include("はすでに存在します")
+      end
+
+      it "emailが重複している場合、登録できない" do
+        # ダミーインスタンス生成・登録
+        user = create(:user)
+        another_user = build(:user, name: "テスト２", email: user.email)
+        # バリデーション実行
+        another_user.valid?
+        # エラーメッセージ生成を確認
+        expect(another_user.errors[:email]).to include("はすでに存在します")
       end
     end
   
